23c3d50f34130be74253c13a52973b6f1265748c
KHALID ALAHMARI <KHALID@KHALIDs-MacBook-Pro.local>
2014-01-22 20:26:47 -0800

    Just Created the Project File

---
+++ AppUsingParseCloud/AppUsingParseCloud/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppUsingParseCloud
+//
+//  Created by KHALID ALAHMARI on 1/22/14.
+//  Copyright (c) 2014 Khalid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
